7b035c813e223e1e2f28d358258c3de75718c516
vad9ss6 <vad9ss6@gmail.com>
2024-02-11 02:47:32 +0300

    Add todo crud

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+} from '@nestjs/common';
+
+import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Todo')
+@ApiBearerAuth()
+@Controller('todo')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Post()
+  create(@Body() createTodoDto: CreateTodoDto, @Req() request) {
+    const { user } = request;
+
+    return this.todoService.create(createTodoDto, user?.sub);
+  }
+
+  @Get()
+  findAll(@Req() request) {
+    const { user } = request;
+
+    return this.todoService.findAll(user?.sub);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.todoService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+    return this.todoService.update(id, updateTodoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.todoService.remove(id);
+  }
+}
