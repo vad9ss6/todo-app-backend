c50f029a1e658f0f04c4741e18a2cf54d5ffb01a
vad9ss6 <vad9ss6@gmail.com>
2024-04-26 12:54:39 +0300

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,10 +7,10 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    console.log('database url', this.configService.get<string>('DATABASE_URL'));
+    console.log('database url', this.configService.get<string>('DATABASE_URL').replace('?sslmode=require', ''));
     return {
       type: 'postgres',
-      url: this.configService.get<string>('DATABASE_URL'),
+      url: this.configService.get<string>('DATABASE_URL').replace('?sslmode=require', ''),
       entities: ['dist/**/**/*.entity.{ts,js}'],
       synchronize: true,
       logging: true,
